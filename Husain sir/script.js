ed164e870e1d8888bfe402f3f1f468f79f697c67
jyoti <sundajyoti062@gmail.com>
2024-08-16 17:05:48 +0530

    my first challenge

---
+++ Husain sir/script.js
@@ -0,0 +1,25 @@
+//  let jyoti=document.getElementById("heading");
+//  console.log(heading);
+
+
+//  let heading =document.getElementsByClassName("jj");
+//  console.log
+
+
+//  let div=document.querySelector("div");
+//  console.dir(div);
+//  div.innerText
+
+// let h2=document.querySelector("h2");
+// console.dir(h2.innerText);
+// h2.innerText=h2.innerText +"from apna colz";
+
+
+// let h5=document.querySelector("h5");
+// console.dir(h5.innerText);
+// h5.innerText=h5.innerText +"mohali chadigrah";
+
+
+let divs = document.querySelectorAll(".box");
+divs(0).innerText="new unique value 1";
+divs(1).innerText ="new unique value 2";
